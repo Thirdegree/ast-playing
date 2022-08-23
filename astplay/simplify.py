feedd21a71624ebc54598ad17ed0dff858e597b2
Joel Christiansen <greenbadge.jc@gmail.com>
2022-08-23 10:35:36 -0600

    support assign, del

--- astplay/simplify.py
+++ astplay/simplify.py
@@ -1,30 +1,50 @@
 import ast
-from typing import Optional, Dict, Mapping
+from typing import Optional, Dict, Mapping, Union
 from pathlib import Path
 
 
 class AccImmutables(ast.NodeVisitor):
-    mut_funcs = set(dir(Dict)) - set(dir(Mapping))  # let the smart people figure out what counts as "mutating"
+    # let the smart people figure out what counts as "mutating"
+    mut_funcs = set(dir(Dict)) - set(dir(Mapping))
     # this one figures out which values are not modified
     # we then need a transformer that uses this, visits function defintions, and modifies whichever args it can
 
     def __init__(self, func: ast.FunctionDef) -> None:
         self.func = func
-        self.are_muted: set[ast.arg] = set()
+        self.are_muted_args: set[ast.arg] = set()
 
     @property
-    def are_not_muted(self) -> set[ast.arg]:
+    def non_muted_args(self) -> set[ast.arg]:
         # this obviously fails to take into account, among other things, kwargs.
-        return set(self.func.args.args) - set(self.are_muted)
+        return set(self.func.args.args) - set(self.are_muted_args)
+
+    def _matching_func_arg(self, name: ast.Name) -> Optional[ast.arg]:
+        return next(n for n in self.func.args.args if n.arg == name.id)
 
     def visit_Attribute(self, node: ast.Attribute) -> None:
         if not isinstance(node.value, ast.Name):
             # don't even know what this case is? so gonna ignore it
             return
-        if (arg := next(n for n in self.func.args.args if n.arg == node.value.id)) is None:
+        if (arg := self._matching_func_arg(node.value)) is None:
             return
         if node.attr in self.mut_funcs:
-            self.are_muted.add(arg)
+            self.are_muted_args.add(arg)
+
+    def _check_modify_mutli(self, node: Union[ast.Assign, ast.Delete]) -> None:
+        for target in node.targets:
+            if not isinstance(target, ast.Subscript):
+                continue
+            if not isinstance(target.value, ast.Name):
+                continue
+            if (arg := self._matching_func_arg(target.value)) is None:
+                continue
+            self.are_muted_args.add(arg)
+
+    def visit_Assign(self, node: ast.Assign) -> None:
+        self._check_modify_mutli(node)
+
+    def visit_Delete(self, node: ast.Delete) -> None:
+        self._check_modify_mutli(node)
 
 
 class FuncReducer(ast.NodeTransformer):
@@ -34,7 +54,7 @@ class FuncReducer(ast.NodeTransformer):
         for arg in node.args.args:
             # for now we assume everything that isn't mutated is a dictionary,
             # because that's definitely a reasonable thing to assume
-            if arg not in mut_checker.are_not_muted:
+            if arg not in mut_checker.non_muted_args:
                 continue
             if arg.annotation is None:
                 continue
@@ -42,7 +62,8 @@ class FuncReducer(ast.NodeTransformer):
                 # again, ignoring the things we don't understand and are too on vacation to research
                 continue
             assert arg.annotation is not None
-            arg.annotation.value = ast.Name(id='Mapping')  # type: ignore[attr-defined]
+            arg.annotation.value = ast.Name(
+                id='Mapping')  # type: ignore[attr-defined]
         return node
 
 
--- tests/test_simplify.py
+++ tests/test_simplify.py
@@ -2,7 +2,7 @@ import ast
 from astplay.simplify import FuncReducer
 
 
-def test_dict_to_mapping() -> None:
+def test_dict_to_mapping_update() -> None:
     code = """\
 def test(muted: dict[str, int], unmuted: dict[int, int]) -> None:
     muted.update({'hello': 5})
@@ -13,4 +13,32 @@ def test(muted: dict[str, int], unmuted: dict[int, int]) -> None:
     assert ast.unparse(result) == """\
 def test(muted: dict[str, int], unmuted: Mapping[int, int]) -> None:
     muted.update({'hello': 5})
+    unmuted.items()"""
+
+
+def test_dict_to_mapping_assign() -> None:
+    code = """\
+def test(muted: dict[str, int], unmuted: dict[int, int]) -> None:
+    muted['hello'] = 5
+    unmuted.items()"""
+
+    result = FuncReducer().visit(ast.parse(code))
+
+    assert ast.unparse(result) == """\
+def test(muted: dict[str, int], unmuted: Mapping[int, int]) -> None:
+    muted['hello'] = 5
+    unmuted.items()"""
+
+
+def test_dict_to_mapping_delete() -> None:
+    code = """\
+def test(muted: dict[str, int], unmuted: dict[int, int]) -> None:
+    del muted['hello']
+    unmuted.items()"""
+
+    result = FuncReducer().visit(ast.parse(code))
+
+    assert ast.unparse(result) == """\
+def test(muted: dict[str, int], unmuted: Mapping[int, int]) -> None:
+    del muted['hello']
     unmuted.items()"""
